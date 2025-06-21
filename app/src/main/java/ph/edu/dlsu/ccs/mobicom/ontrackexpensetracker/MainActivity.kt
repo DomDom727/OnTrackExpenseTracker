c468aa05a06f2b68dadb2003eb6494e9a84f6c13
DomDom727 <62593436+DomDom727@users.noreply.github.com>
2025-06-21 13:50:45 +0800

    Initial Commit

---
+++ app/src/main/java/ph/edu/dlsu/ccs/mobicom/ontrackexpensetracker/MainActivity.kt
@@ -0,0 +1,47 @@
+package ph.edu.dlsu.ccs.mobicom.ontrackexpensetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ph.edu.dlsu.ccs.mobicom.ontrackexpensetracker.ui.theme.OnTrackExpenseTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OnTrackExpenseTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OnTrackExpenseTrackerTheme {
+        Greeting("Android")
+    }
+}
