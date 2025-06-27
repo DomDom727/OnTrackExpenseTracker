ce2ffd0cd06828ec2cbe958ea82abae31959b57a
DomDom727 <62593436+DomDom727@users.noreply.github.com>
2025-06-28 03:59:37 +0800

    added working onClickListener to move from MainActivity to TransactionsActivity

--- app/src/main/java/ph/edu/dlsu/ccs/mobicom/ontrackexpensetracker/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/ccs/mobicom/ontrackexpensetracker/MainActivity.kt
@@ -1,10 +1,12 @@
 package ph.edu.dlsu.ccs.mobicom.ontrackexpensetracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ph.edu.dlsu.ccs.mobicom.ontrackexpensetracker.R
 
 
 class MainActivity : ComponentActivity() {
@@ -19,5 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val transactionsCardView: CardView = findViewById(R.id.cardView3)
+        transactionsCardView.setOnClickListener {
+            val intent = Intent(this, TransactionsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ph/edu/dlsu/ccs/mobicom/ontrackexpensetracker/TransactionsActivity.kt
@@ -0,0 +1,12 @@
+package ph.edu.dlsu.ccs.mobicom.ontrackexpensetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class TransactionsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transactions)
+    }
+}
